33540e5328939d3add73880117083c4d406de897
insanepure <insanepure@gmail.com>
2021-02-01 08:29:34 +0100

    remove useless files

--- db-bg/ipbanned.php
+++
@@ -1,9 +0,0 @@
-<?php $blacklist = array("162.158.91.238", "456.789.123", "789.123.456");
-
-if(in_array($_SERVER['REMOTE_ADDR'], $blacklist)) {
-
-	//header("Location: https://www.google.de");
-
-	//exit();
-} 
-?>
